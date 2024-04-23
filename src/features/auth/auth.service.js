a604af39d772648ced168216c2f882b43bc4282b
Emmanuel <okponobiemmanuel@gmail.com>
2024-04-23 08:51:25 +0100

    Update backend integratio api by adding it as an env variable in .env file and removing proxy object in package.json file

--- src/features/auth/auth.service.js
+++ src/features/auth/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/';
+const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api/users/`;
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
--- src/features/tasks/tasks.service.js
+++ src/features/tasks/tasks.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = '/api/tasks/';
+const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api/tasks/`;
 
 const createTask = async (taskData, token) => {
   const config = {
